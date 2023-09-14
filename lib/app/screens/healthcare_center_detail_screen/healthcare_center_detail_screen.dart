7ca89342fed5a5b63831abe93a1ac01fabea8e05
bhavik <kotharibhavik23@gmail.com>
2023-09-14 11:37:19 +0530

    final updates

--- lib/app/screens/healthcare_center_detail_screen/healthcare_center_detail_screen.dart
+++ lib/app/screens/healthcare_center_detail_screen/healthcare_center_detail_screen.dart
@@ -9,10 +9,12 @@ import 'package:whizz/app/utils/colors.dart';
 import 'package:whizz/app/widgets/custom_icon_button.dart';
 import '../../utils/constants.dart';
 import '../../utils/scale_utility.dart';
+import '../../widgets/custom_card_widget.dart';
+import '../../widgets/custom_search_field.dart';
+import '../../widgets/tab_indicator.dart';
 import 'controller/healthcare_controller.dart';
 
-// class HealthcareCenterDetailScreen extends GetView<HealthcareController> {
-class HealthcareCenterDetailScreen extends StatelessWidget {
+class HealthcareCenterDetailScreen extends GetView<HealthcareController> {
   const HealthcareCenterDetailScreen({super.key});
 
   @override
@@ -20,72 +22,148 @@ class HealthcareCenterDetailScreen extends StatelessWidget {
     ScalingUtility scale = ScalingUtility(context: context)
       ..setCurrentDeviceSize();
     return Scaffold(
+      backgroundColor: Colors.white,
       body: DefaultTabController(
         length: 6,
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: Colors.white,
-              automaticallyImplyLeading: false,
-              title: Row(
-                children: [
-                  CustomIconButton(
-                    image: ConstantData.backArrow,
-                    height: scale.getScaledHeight(45),
-                    width: scale.getScaledWidth(45),
-                    onTap: () {
-                      // Get.back();
-                    },
-                  ),
-                  SizedBox(
-                    width: scale.getScaledWidth(10),
-                  ),
-                  SizedBox(
-                    width: scale.getScaledWidth(260),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Hospital Name',
-                          style: AppTheme.appTheme.textTheme.titleLarge,
-                        ),
-                        Row(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: scale.getScaledHeight(30),
+              ),
+              ListTile(
+                title: Row(
+                    children: [
+                      CustomIconButton(
+                        image: ConstantData.backArrow,
+                        height: scale.getScaledHeight(45),
+                        width: scale.getScaledWidth(45),
+                        onTap: () {
+                          // Get.back();
+                        },
+                      ),
+                      SizedBox(
+                        width: scale.getScaledWidth(10),
+                      ),
+                      SizedBox(
+                        width: scale.getScaledWidth(260),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              '4.5/5 ',
-                              style: AppTheme.appTheme.textTheme.titleSmall,
+                              'Hospital Name',
+                              style: AppTheme.appTheme.textTheme.titleLarge,
                             ),
-                            RatingBar.builder(
-                              initialRating: 4.5,
-                              minRating: 1,
-                              direction: Axis.horizontal,
-                              allowHalfRating: true,
-                              itemCount: 5,
-                              itemSize: scale.getScaledFont(20),
-                              unratedColor: ColorsUtil.lightContainerColor,
-                              itemPadding: scale.getPadding(horizontal: 2),
-                              itemBuilder: (context, _) => Icon(
-                                Icons.star,
-                                color: ColorsUtil.darkContainerColor,
-                              ),
-                              onRatingUpdate: (rating) {
+                            Row(
+                              children: [
+                                Text(
+                                  '4.5/5 ',
+                                  style: AppTheme.appTheme.textTheme.titleSmall,
+                                ),
+                                RatingBar.builder(
+                                  initialRating: 3.5,
+                                  minRating: 1,
+                                  direction: Axis.horizontal,
+                                  allowHalfRating: true,
+                                  itemCount: 5,
+                                  itemSize: scale.getScaledFont(20),
+                                  unratedColor: ColorsUtil.lightContainerColor,
+                                  itemPadding: scale.getPadding(horizontal: 2),
+                                  itemBuilder: (context, _) => Icon(
+                                    Icons.star,
+                                    color: ColorsUtil.darkContainerColor,
+                                  ),
+                                  onRatingUpdate: (rating) {
 
-                              },
-                            )
+                                  },
+                                )
+                              ],
+                            ),
                           ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
               ),
-              toolbarHeight: 70,
-              pinned: true,
-              expandedHeight: scale.getScaledHeight(170),
-              elevation: 0,
-              bottom: PreferredSize(
-                preferredSize: const Size(double.maxFinite, 60),
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 400),
+                child: true ? Padding(
+                  padding: scale.getPadding(left: 20, right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: scale.getScaledHeight(20),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_pin,
+                            color: Colors.red,
+                            size: scale.getScaledFont(18),
+                          ),
+                          SizedBox(
+                            width: scale.getScaledWidth(5),
+                          ),
+                          SizedBox(
+                            width: scale.getScaledWidth(280),
+                            child: Text(
+                              'Indore, Madhya Pradesh',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: AppTheme.appTheme.textTheme.titleSmall!.copyWith(
+                                fontWeight: FontWeight.w500,
+                                fontSize: scale.getScaledFont(14),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: scale.getScaledHeight(8),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.calendar_month,
+                            color: Colors.red,
+                            size: scale.getScaledFont(18),
+                          ),
+                          SizedBox(
+                            width: scale.getScaledWidth(5),
+                          ),
+                          SizedBox(
+                            width: scale.getScaledWidth(280),
+                            child: Text(
+                              'Open From ${DateFormat('hh : mm a').format(DateTime.now())} To ${DateFormat('hh : mm a').format(DateTime.now())}',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: AppTheme.appTheme.textTheme.titleSmall!.copyWith(
+                                fontWeight: FontWeight.w500,
+                                fontSize: scale.getScaledFont(14),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ) : Container(),
+              ),
+              SizedBox(
+                height: scale.getScaledHeight(15),
+              ),
+              Container(
+                height: scale.getScaledHeight(42),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 2,
+                      color: Colors.grey[300]!,
+                    )
+                  )
+                ),
                 child: TabBar(
                   tabs: const [
                     Tab(
@@ -115,126 +193,29 @@ class HealthcareCenterDetailScreen extends StatelessWidget {
                     fontWeight: FontWeight.w500,
                     fontSize: scale.getScaledFont(16),
                   ),
+                  indicator: TabIndicator(),
                   indicatorColor: ColorsUtil.darkContainerColor,
                   unselectedLabelColor: Colors.black,
                 ),
               ),
-              flexibleSpace: FlexibleSpaceBar(
-                background: Padding(
-                  padding: scale.getPadding(left: 20, right: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: scale.getScaledHeight(105),
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.location_pin,
-                            color: Colors.red,
-                            size: scale.getScaledFont(18),
-                          ),
-                          SizedBox(
-                            width: scale.getScaledWidth(5),
-                          ),
-                          SizedBox(
-                            width: scale.getScaledWidth(280),
-                            child: Text(
-                              'Indore, Madhya Pradesh',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: AppTheme.appTheme.textTheme.titleSmall!.copyWith(
-                                fontWeight: FontWeight.w500,
-                                fontSize: scale.getScaledFont(14),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: scale.getScaledHeight(5),
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.calendar_month,
-                            color: Colors.red,
-                            size: scale.getScaledFont(18),
-                          ),
-                          SizedBox(
-                            width: scale.getScaledWidth(5),
-                          ),
-                          SizedBox(
-                            width: scale.getScaledWidth(280),
-                            child: Text(
-                              'Open From ${DateFormat('hh : mm a').format(DateTime.now())} To ${DateFormat('hh : mm a').format(DateTime.now())}',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: AppTheme.appTheme.textTheme.titleSmall!.copyWith(
-                                fontWeight: FontWeight.w500,
-                                fontSize: scale.getScaledFont(14),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Container(
+              Container(
                 color: Colors.white,
                 margin: scale.getMargin(horizontal: 15),
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Visibility(
-                      visible: false,
-                      child: TabBar(
-                        tabs: const [
-                          Tab(
-                            text: 'Basic Details',
-                          ),
-                          Tab(
-                            text: 'Services',
-                          ),
-                          Tab(
-                            text: 'Infrastructure',
-                          ),
-                          Tab(
-                            text: 'Medicine Stock',
-                          ),
-                          Tab(
-                            text: 'Doctor Details',
-                          ),
-                          Tab(
-                            text: 'Reviews & Feedback',
-                          ),
-                        ],
-                        isScrollable: true,
-                        indicatorWeight: 4,
-                        indicatorSize: TabBarIndicatorSize.label,
-                        labelColor: ColorsUtil.darkContainerColor,
-                        labelStyle: AppTheme.appTheme.textTheme.titleSmall!.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: scale.getScaledFont(16),
-                        ),
-                        indicatorColor: ColorsUtil.darkContainerColor,
-                        unselectedLabelColor: Colors.black,
-                      ),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height,
+                    AnimatedContainer(
+                      duration: const Duration(milliseconds: 200),
+                      height: true
+                          ? MediaQuery.of(context).size.height*0.70
+                          : MediaQuery.of(context).size.height*0.79,
                       child: TabBarView(
                         children: [
                           Container(),
                           Container(),
                           Container(),
                           Container(),
-                          Container(),
+                          _doctorListTab(scale, context),
                           Container(),
                         ],
                       ),
@@ -242,10 +223,86 @@ class HealthcareCenterDetailScreen extends StatelessWidget {
                   ],
                 ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  _doctorListTab(ScalingUtility scale, context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: scale.getScaledHeight(15),
+        ),
+        CustomSearchField(
+          height: scale.getScaledHeight(65),
+          width: scale.getScaledWidth(320),
+          suffixIcon: IconButton(
+            onPressed: () {
+
+            },
+            icon: Icon(
+              Icons.search,
+              size: scale.getScaledFont(30),
+            ),
+          ),
+          hintText: 'Search Here',
+        ),
+        MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          child: Expanded(
+            child: ListView.builder(
+              itemCount: 50,
+              primary: false,
+              itemBuilder: (context, index){
+                return CustomCardWidget(
+                  padding: scale.getPadding(horizontal: 15.0, vertical: 10.0,),
+                  margin: scale.getMargin(horizontal: 5.0, vertical: 6.0,),
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.person,
+                        color: Colors.black,
+                      ),
+                      SizedBox(
+                        width: scale.getScaledWidth(20),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Doctor Name',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: AppTheme.appTheme.textTheme.titleSmall!.copyWith(
+                              fontWeight: FontWeight.w500,
+                              fontSize: scale.getScaledFont(16),
+                            ),
+                          ),
+                          Text(
+                            'Doctors Description',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: AppTheme.appTheme.textTheme.titleSmall!.copyWith(
+                              fontWeight: FontWeight.w400,
+                              fontSize: scale.getScaledFont(13),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/app/widgets/custom_search_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:whizz/app/utils/colors.dart';
+
+class CustomSearchField extends StatelessWidget {
+  final Color? color;
+  final double? height;
+  final double? width;
+  final String? hintText;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+  final BorderRadiusGeometry? borderRadius;
+  final Function(String)? onChange;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  const CustomSearchField({
+    this.prefixIcon,
+    this.suffixIcon,
+    Key? key,
+    this.hintText,
+    this.color,
+    this.height,
+    this.width,
+    this.onChange,
+    this.padding,
+    this.margin,
+    this.borderRadius}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height?? 30,
+      width: width?? 200,
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: hintText,
+          suffixIcon: suffixIcon,
+          prefixIcon: prefixIcon,
+        ),
+      ),
+    );
+  }
+}
