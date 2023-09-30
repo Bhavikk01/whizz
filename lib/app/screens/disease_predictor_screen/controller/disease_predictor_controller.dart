f1bd5f11edb9ac1b7c81729a05eb04c0afcc503f
daksh <dakshpathak0705@gmail.com>
2023-10-01 00:24:03 +0530

    done with disease_predictor_screen.dart frontend

--- lib/app/screens/disease_predictor_screen/controller/disease_predictor_controller.dart
+++ lib/app/screens/disease_predictor_screen/controller/disease_predictor_controller.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class PredictorController extends GetxController
 {
-  
+  TextEditingController controller = TextEditingController();
 }
